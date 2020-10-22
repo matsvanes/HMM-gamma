dd7fd662b6c7e39187695fb630a2626e4cb4b20d
Mats van Es <gebruiker@Matss-MacBook-Pro.local>
2020-10-22 12:03:43 +0100

    change M1 TF analysis, organize and clean up.

--- hmm_gamma_analysis.m
+++ hmm_gamma_analysis.m
@@ -24,20 +24,20 @@ end
 if exist('/ohba/pi/mwoolrich/', 'dir'), islocal=0; else, islocal=1; end
 if islocal
   PATH_BASE = '/Volumes/T5_OHBA/'; % if on a local desktop
-  warning('PATH_ORIG not accessible')
   PATH_ORIG = [];
-  islocal = 1;
+  islocal   = 1;
+  warning('PATH_ORIG not accessible')
 else
   PATH_BASE = '/ohba/pi/mwoolrich/';
   PATH_ORIG = [PATH_BASE, 'datasets/CZ_Gamma/MEG_Gamma_HandOver/'];
   PATH_BASE = [PATH_BASE, 'mvanes/'];
-  islocal = 0;
+  islocal   = 0;
 end
 PATH_ANALYSIS = [PATH_BASE, 'analysis/HMM-gamma/'];
-PATH_DATA =  [PATH_ANALYSIS 'data/'];
+PATH_DATA     = [PATH_ANALYSIS 'data/'];
 PATH_ORIGDATA = [PATH_ORIG, 'data/'];
-PATH_TF = [PATH_ANALYSIS, 'TF/'];
-PATH_HMM = [PATH_ANALYSIS 'HMM/'];
+PATH_TF       = [PATH_ANALYSIS, 'TF/'];
+PATH_HMM      = [PATH_ANALYSIS 'HMM/'];
 
 PATH_SCRIPT = [PATH_BASE, 'scripts/HMM-gamma/'];
 
@@ -45,21 +45,19 @@ files=dir([PATH_DATA 'efd_*.mat']);
 
 %% PARAMS
 
-time_bl     = [-1 -0.5];
-
-
 % TF
 keep = 0;
 megtype = 'neuromag_sss';
-freq = 5:1:145;
-res = 2.5;
+if ~exist('time_bl', 'var'), time_bl = [-1 -0.5]; end
+if ~exist('freq',    'var'), freq    = 5:1:145;   end
+if ~exist('res',     'var'), res     = 2.5;       end
 sensor = 0;
 
 % HMM
 round_factor = 1000;
-order = 5;
-N_states = [3:1:10];
-realization = [1:10];
+order        = 5;
+N_states     = [3:1:10];
+realization  = [1:10];
 
 if ~exist('subs', 'var'), subs = 1:length(files); end
 
@@ -70,7 +68,6 @@ if run.TF.run
   for rois = 1:numel(run.ROI)
     tmpPATH_TF = [PATH_TF, run.ROI{rois}, '/'];
     
-    TF_avg = [];
     for s = subs
       s
       [D, POI] = hmm_gamma_preparedata(PATH_DATA, tmpPATH_TF, files(s).name, run.ROI{rois}, run.remove_parc);
@@ -121,81 +118,57 @@ if run.TF.run
         S.circularise = false;
         D = spm_eeg_average(S);
         if ~keep, delete(S.D);  end
-      end
-      
-      % Log
-      S = [];
-      S.D = D;
-      if strcmp(run.ROI{rois}, 'M1')
-        S.method = 'Diff';
-      else
+        
+        % Log
+        S = [];
+        S.D = D;
         S.method = 'LogR';
+        S.timewin = [time_bl(1)*1000 time_bl(2)*1000];
+        S.pooledbaseline = 1;
+        D = spm_eeg_tf_rescale(S);
       end
-      S.timewin = [time_bl(1)*1000 time_bl(2)*1000];
-      S.pooledbaseline = 1;
-      D = spm_eeg_tf_rescale(S);
-      if strcmp(run.ROI{rois}, 'parc')
-        if run.remove_parc
-          TF_avg(s,:,:)=squeeze(D(14,:,:,:)); %POI = 14; % Precentral after removal of some ROIs, otherwise POI=23;
-        else
-          TF_avg(s,:,:)=squeeze(D(23,:,:,:));
-        end
-      elseif strcmp(run.ROI{rois}, 'sensor')
-        TF_avg(s,:,:,:)=D(:,:,:);
-      end
-    end
-    
-    if strcmp(run.ROI{rois}, 'parc') || strcmp(run.ROI{rois}, 'sensor')
-      save([tmpPATH_TF 'TF_avg.mat'],'TF_avg','files');
     end
   end
 end
 
 %% EVAL TF
 if  run.TF.eval
-  load ([PATH_TF 'TF_avg.mat']);
-  % visulaisation (GA,SS)
-  meg_tms_tf_vis(TF_avg,D,freq,time_bl,files,PATH_ORIGDATA,PATH_TF)
-  keyboard;
-  % peak frequency
-  [freq_peak,power]=meg_tms_tf_peak(TF_avg,D,freq,time_pre,time_peri,time_post,0,1,1);
-  % eval and corr
-  meg_tms_tf_eval(PATH_BASE,PATH_TF,files,freq_peak,power)
+  plot_M1_TF
 end
 
 %% PREP HMM
 if run.HMM.prep
-files=dir([PATH_DATA 'efd_*.mat']);
-for rois = 1:numel(run.ROI)
+  files=dir([PATH_DATA 'efd_*.mat']);
+  for rois = 1:numel(run.ROI)
     tmpPATH_HMM = [PATH_HMM, run.ROI{rois}, '/'];
-  for s = subs
-    [D, POI, dat] = hmm_gamma_preparedata(PATH_DATA, tmpPATH_TF, files(s).name, run.ROI{rois}, run.remove_parc);
-
-    PAC{s} = squeeze(dat(POI,:,:));
-    t{s} = D.time(1):1/D.fsample:D.time(end);
-    t{s} = round(t{s}*round_factor)/round_factor; % ensure that it's rounded properly for 'find' function
+    for s = subs
+      [D, POI, dat] = hmm_gamma_preparedata(PATH_DATA, tmpPATH_TF, files(s).name, run.ROI{rois}, run.remove_parc);
+      
+      PAC{s} = squeeze(dat(POI,:,:));
+      t{s} = D.time(1):1/D.fsample:D.time(end);
+      t{s} = round(t{s}*round_factor)/round_factor; % ensure that it's rounded properly for 'find' function
+      
+      % PC_1st: 1xlength(files) cell, whereby each cell contains a samples(currently 301)xtrials matrix
+      nsamples{s} = size(PAC{s},1);
+      ntrials{s} = size(PAC{s},2);
+      
+      %format for HMM
+      X{s}(:,1) = PAC{s}(:); % change format. Within each subject/file cell from matrix (smaplesx trials) to vector (concatenate all trials)
+      T{s} = repmat(nsamples{s},1,ntrials{s})';
+    end %subj
+    save([PATH_HMM 'PREP_HMM.mat'],'X','T','ntrials','t','round_factor','order','D');
     
-    % PC_1st: 1xlength(files) cell, whereby each cell contains a samples(currently 301)xtrials matrix
-    nsamples{s} = size(PAC{s},1);
-    ntrials{s} = size(PAC{s},2);
-    
-    %format for HMM
-    X{s}(:,1) = PAC{s}(:); % change format. Within each subject/file cell from matrix (smaplesx trials) to vector (concatenate all trials)
-    T{s} = repmat(nsamples{s},1,ntrials{s})';
-  end %subj
-  save([PATH_HMM 'PREP_HMM.mat'],'X','T','ntrials','t','round_factor','order','D');
-  
-  % MAR check
-  X_all=cell2mat(X');
-  mkdir([PATH_HMM 'order_check/'])
-  cd([PATH_SCRIPT 'spectra_check/']);
-  for oo=1:20
-    hmmmar_spectra_check(X_all, oo, D.fsample);
-    print('-dpng',[PATH_HMM,'order_check/order_',sprintf('%03d.png',oo)]);
-    close(gcf)
-  end
-  hmmmar_spectra_check(X_all, order, D.fsample);
-end %if prep HMM
+    % MAR check
+    X_all=cell2mat(X');
+    mkdir([PATH_HMM 'order_check/'])
+    cd([PATH_SCRIPT 'spectra_check/']);
+    for oo=1:20
+      hmmmar_spectra_check(X_all, oo, D.fsample);
+      print('-dpng',[PATH_HMM,'order_check/order_',sprintf('%03d.png',oo)]);
+      close(gcf)
+    end
+    hmmmar_spectra_check(X_all, order, D.fsample);
+  end %if prep HMM
 end
 
 %% RUN HMMs
