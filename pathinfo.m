ec042afc5163c52d3e0149928ed58f86de728560
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-01-19 16:46:05 +0100

    add local startup file

--- pathinfo.m
+++ pathinfo.m
@@ -3,6 +3,7 @@ islocal = isfolder('/Volumes/T5_OHBA/');
 if islocal
   projectdir = '/Volumes/T5_OHBA/';
   homedir = '/Volumes/T5_OHBA/';
+  startupdir = '/Users/matsvanes/Documents/MATLAB/startup.m';
 else
   projectdir = '/ohba/pi/mwoolrich/mvanes/';
   homedir = '/home/mvanes/';
