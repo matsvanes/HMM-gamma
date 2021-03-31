b6cd9d4b0def898aa7d41f4d3d427c63b6225ee3
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-03-31 15:12:51 +0200

    by default, don't save individual trials

--- tftosub.m
+++ tftosub.m
@@ -1,4 +1,4 @@
-function [tf, tf_avg] = tftosub(tf, T)
+function [tf, tf_avg] = tftosub(tf, T, keeptrials)
 % intended to restructure the HMM's tf spectra (time * freq) to a
 % time*freq*trial matrix per subject
 %
@@ -7,11 +7,14 @@ function [tf, tf_avg] = tftosub(tf, T)
 % Input
 % tf: matrix time * freq, output of hmmtimefreq
 % T: cell (#sub) of amount of samples per trial
+% keeptrials: boolean, whether to keep individual trials or subject average
 %
 % Output
-% tf: cell (#sub), matrix of time*freq*trial
+% tf: cell (#sub), matrix of time*freq(*trial)
 % tf_avg: matrix time*freq group average tf
+
 if ndims(tf)>2, error('this function does not yet work on multichannel data'), end
+if nargin<3 || isempty('keeptrials'), keeptrials=false; end
 
 for ii=1:numel(T)
   t0 = sum(cat(1,T{1:ii}))-sum(cat(1,T{ii}));
@@ -21,7 +24,11 @@ for ii=1:numel(T)
   end
   tf_avgTr{ii} = nanmean(tf_tmp{ii},3);
 end
-tf = tf_tmp;
+if keeptrials
+  tf = tf_tmp;
+else
+  tf = tf_avgTr;
+end
 tf_avgTr = permute(cat(3, tf_avgTr{:}), [3 1 2]);
 
 % group TF
