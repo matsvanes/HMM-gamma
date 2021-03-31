b9e3c1961961b1382bb04117bb5fb09072d66d6a
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-03-31 12:49:05 +0200

    put TF computation in try - catch

--- hmm_gamma_hmm_post.m
+++ hmm_gamma_hmm_post.m
@@ -1,4 +1,4 @@
-function [Gamma, MLGamma, dynamics, spectra, tf, tfconvol, tf_avgT_avgS] = hmm_gamma_hmm_post(X, Gamma, hmm, T, time, options, doplot, filename)
+function [Gamma, MLGamma, dynamics, spectra, spectramt, tf, tfmt, tf_avg, tfmt_avg] = hmm_gamma_hmm_post(X, Gamma, hmm, T, time, options, doplot, filename)
 if ~exist('doplot', 'var'), doplot=1; end
 if contains(filename, '.mat'), filename = extractBefore(filename, '/mat'); end
 
@@ -24,70 +24,37 @@ dynamics.SwitchingRate =  getSwitchingRate(Gamma,T,hmm.train); % rate of switchi
 MLGamma = gammaRasterPlot(Gamma_pad, cat(1,T{:}));
 MLGamma = reshape(MLGamma,T{1}(1),[]);
 
+try
 % State spectra
 if options.order>0
   spectra = hmmspectramar(cat(1,X{:}), cat(1,T{:}), hmm, Gamma, options);
 else
-  spectra = hmmspectramt(cat(1,X{:}), cat(1,T{:}), Gamma, options);
+  spectra = hmmspectratde(hmm, options);
 end
+spectramt = hmmspectramt(cat(1,X{:}), cat(1,T{:}), Gamma, options);
 
 % reshape Gamma into ntime*ntrl*nstates
 Gamma = reshape(Gamma,T{1}(1)-options.order-numel(options.embeddedlags)+1,[],options.K);
 
-% HMM based time-frequency representation
-tf = hmmtimefreq(spectra, Gamma_pad, 1);
+% Time-frequency decomposition
+% tf = hmmtimefreq(spectra, Gamma_pad, 0); % based on HMM parameters
+tfmt = hmmtimefreq(spectramt, Gamma_pad, 0); % based on multitapers
 
 % psd per subject
-for ii=1:numel(T)
-  t0 = sum(cat(1,T{1:ii}))-sum(cat(1,T{ii}));
-  tf_tmp{ii} = zeros(max(cat(1,T{:})), size(tf,2), length(T{ii}));
-  for jj=1:length(T{ii})
-    tf_tmp{ii}(:,:,jj)=tf(t0+sum(T{ii}(1:jj))-sum(T{ii}(jj)) +1 : t0+ sum(T{ii}(1:jj)),:);
-  end
-  tf_avgTr{ii} = nanmean(tf_tmp{ii},3);
-end
-tf = tf_tmp;
-tf_avgTr = permute(cat(3, tf_avgTr{:}), [3 1 2]);
+[tf, tf_avg] = tftosub(tf, T, false);
+[tfmt, tfmt_avg] = tftosub(tfmt, T, false);
 
-% group TF
-tf_avgT_avgS = squeeze(nanmean(tf_avgTr,1));
-
-% Compute TF using mtmconvol
-timeidx = options.order+numel(options.embeddedlags);
-time_short = time(timeidx:end); 
-cfg.foi = 1:100;
-tfconvol = hmmtimefreqconvol(cfg, Gamma, T, time_short, dynamics.F0, 1);
 
 if doplot
-  hmm_post_plot(options, T, time, dynamics.F0, MLGamma, spectra, tf_avgT_avgS, tfconvol);
+  hmm_post_plot(options, T, time, dynamics.F0, MLGamma, spectra, spectramt, tf_avg, tfmt_avg);
   if ~exist('filename', 'var') || isempty(filename)
     filename = 'HMM';
   end
   print('-dpng',filename);pause(1);
 end
-% factorisation into frequency bands
-%{
-if 1 % manual bands
-  bands = [0 4; 4 8; 8 13; 14 30; 30 60; 60 inf];
-  sp_fit = spectbands(spectra,bands);
-else
-  % or if using data-driven bands: FIXME:MIGHT NOT WORK? --> use cov
-  Gamma = padGamma(Gamma,T,options);
-  nsubjects=numel(T);
-  sp_fit_subj = cell(nsubjects,1); Tacc = 0;
-  for jj = 1:nsubjects
-    ind = Tacc + (1:sum(T{jj})); Tacc = Tacc + length(ind);
-    sp_fit_subj{jj} = hmmspectramt(X{jj},T{jj},Gamma(ind,:),options);
-  end
-  params_fac = struct();
-  params_fac.Method = 'NNMF';
-  params_fac.Ncomp = 4;
-  params_fac.Base = 'psd';
-  [spectral_factors,spectral_profiles,spectral_factors_subj] = spectdecompose(sp_fit_subj,params_fac);
+
+catch
+  Gamma = reshape(Gamma,T{1}(1)-options.order-numel(options.embeddedlags)+1,[],options.K);
+  spectra=[]; spectramt=[]; tf=[]; tfmt=[]; tf_avg=[]; tfmt_avg=[];
 end
-%}
-
-
-  
-
 end
