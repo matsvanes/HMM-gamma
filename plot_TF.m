e85d20a5f11f9485c8c192c1e25fa689b0a2455e
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-03-31 15:13:43 +0200

    add optional input params

--- plot_TF.m
+++ plot_TF.m
@@ -1,9 +1,19 @@
-function h = plot_TF(t, f, tf, titl)
+function h = plot_TF(t, f, tf, titl, clim, cmap)
 
-cmap = flipud(brewermap(64, 'RdBu'));
-if ~exist('title', 'var'), titl=[]; end
+if numel(t)==size(tf,1) && numel(f)==size(tf,2)
+  tf=tf';
+end
 
-imagesc(t, f, tf);
+if ~exist('cmap', 'var') || isempty(cmap)
+  cmap = flipud(brewermap(64, 'RdBu'));
+end
+if ~exist('titl', 'var'), titl=[]; end
+
+if exist('clim', 'var') && ~isempty(clim)
+  imagesc(t, f, tf, clim);
+else
+  imagesc(t, f, tf);
+end
 colormap(cmap)
 set(gca,'YDir','normal');
 xlabel('Time (s)');ylabel('Frequency (Hz)');
